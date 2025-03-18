6ece2db824d4f1fe4a10ce3f5f520d4ff29b2355
Richa Jos <richajos34@berkeley.edu>
2025-03-17 20:15:15 -0700

    swicthed pfp

--- frontend/src/app/navbar/page.tsx
+++ frontend/src/app/navbar/page.tsx
@@ -6,17 +6,7 @@ const Navbar = ({ profilePicUrl }: { profilePicUrl: string }) => {
   return (
     <nav className="bg-white py-4 px-6 flex justify-between items-center border-b">
       {/* Logo Placeholder */}
-      <div className="w-12 h-12 bg-gray-300 rounded-full">
-        {profilePicUrl !== '' && (
-          <Image 
-            className="rounded-full"
-            src={profilePicUrl}
-            width={50}
-            height={50}
-            alt={'profile image'}
-          />
-        )}
-      </div>
+      <div className="w-12 h-12 bg-gray-300 rounded-full flex items-center justify-center text-xs text-gray-60"><Link href="/profile">logo</Link></div>
 
       {/* Navigation Links */}
       <ul className="flex space-x-8 text-gray-700 font-medium">
@@ -28,8 +18,19 @@ const Navbar = ({ profilePicUrl }: { profilePicUrl: string }) => {
         <li className="hover:text-black cursor-pointer">PRICING</li>
         <li className="hover:text-black cursor-pointer">MORE</li>
         <li className="hover:text-black cursor-pointer">CONTACT</li>
-        <div className="w-12 h-12 bg-gray-300 rounded-full flex items-center justify-center text-xs text-gray-60"><Link href="/profile">profile</Link></div>
       </ul>
+
+      <div className="w-12 h-12 bg-gray-300 rounded-full">
+        {profilePicUrl !== '' && (
+          <Image 
+            className="rounded-full"
+            src={profilePicUrl}
+            width={50}
+            height={50}
+            alt={'profile image'}
+          />
+        )}
+      </div>
 
     </nav>
   );
