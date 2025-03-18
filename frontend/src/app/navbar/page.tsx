d60d43d3390e47c19e05cfcee5e4b54d3bec4170
Richa Jos <richajos34@berkeley.edu>
2025-03-17 20:18:13 -0700

    reordered pfp

--- frontend/src/app/navbar/page.tsx
+++ frontend/src/app/navbar/page.tsx
@@ -9,18 +9,14 @@ const Navbar = ({ profilePicUrl }: { profilePicUrl: string }) => {
       <div className="w-12 h-12 bg-gray-300 rounded-full flex items-center justify-center text-xs text-gray-60"><Link href="/profile">logo</Link></div>
 
       {/* Navigation Links */}
-      <ul className="flex space-x-8 text-gray-700 font-medium">
-        <li className="hover:text-black cursor-pointer">
-          <Link href="/dashboard">HOME</Link>
-        </li>
+      <ul className="flex space-x-8 text-gray-700 font-medium items-center">
+        <li className="hover:text-black cursor-pointer"><Link href="/dashboard">HOME</Link></li>
         <li className="hover:text-black cursor-pointer">SERVICES</li>
         <li className="hover:text-black cursor-pointer"><Link href="/form">ORDER FORMS</Link></li>
         <li className="hover:text-black cursor-pointer">PRICING</li>
         <li className="hover:text-black cursor-pointer">MORE</li>
         <li className="hover:text-black cursor-pointer">CONTACT</li>
-      </ul>
-
-      <div className="w-12 h-12 bg-gray-300 rounded-full">
+        <div className="w-12 h-12 bg-gray-300 rounded-full flex items-center justify-center">
         {profilePicUrl !== '' && (
           <Image 
             className="rounded-full"
@@ -31,6 +27,7 @@ const Navbar = ({ profilePicUrl }: { profilePicUrl: string }) => {
           />
         )}
       </div>
+      </ul>
 
     </nav>
   );
